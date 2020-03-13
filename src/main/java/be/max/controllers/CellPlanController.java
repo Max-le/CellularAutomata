f2537b7fc648a7fe693c573b1a48efc8e9d630ed
Max <max.09@outlook.xom>
2020-03-13 09:31:37 +0100

    enable CORS

--- src/main/java/be/max/controllers/CellPlanController.java
+++ src/main/java/be/max/controllers/CellPlanController.java
@@ -7,6 +7,7 @@ import be.max.Plan;
 import be.max.mappers.PlanMapper;
 import be.max.representations.CellPlan;
 import be.max.rules.Rule250;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CellPlanController {
 
+    @CrossOrigin
     @GetMapping("")
     public CellPlan cellPlan(@RequestParam(value ="rule") String rule,
-                             @RequestParam(value="numberRows") int nbRows,
-                             @RequestParam(value="lengthRow") int lengthRow){
+                             @RequestParam(value="number") int nbRows,
+                             @RequestParam(value="length") int lengthRow){
         //TODO Mapper le rule String en rule de type Rule
         Plan plan = Iterator.generatePlan(Generator.oneCellRow(lengthRow), nbRows, new Rule250());
         PlanMapper mapper = new PlanMapper();
